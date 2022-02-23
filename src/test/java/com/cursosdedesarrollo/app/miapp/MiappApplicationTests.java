04eb65b8f8d3e9e8f9d0cd7d0e9a1ec49c5099d5
David Vaquero <pepesan@gmail.com>
2022-02-23 19:12:09 +0100

    ejemplos de controladores, paso de parámetros a plantilla,uso de thymeleaf para plantillas, integración con css propio y con webjars

--- src/test/java/com/cursosdedesarrollo/app/miapp/MiappApplicationTests.java
+++ src/test/java/com/cursosdedesarrollo/app/miapp/MiappApplicationTests.java
@@ -2,12 +2,31 @@ package com.cursosdedesarrollo.app.miapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
-class MiappApplicationTests {
+@AutoConfigureMockMvc
+public class MiappApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
-	void contextLoads() {
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
 	}
-
 }
+
