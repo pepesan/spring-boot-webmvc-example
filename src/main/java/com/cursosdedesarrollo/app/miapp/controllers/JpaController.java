d2e244e16c7d0d41b8c7b91d7557b749fef87496
David Vaquero <pepesan@gmail.com>
2022-02-28 12:24:29 +0100

    manejo de datos en jpa con entidad simple, show, update delete

--- src/main/java/com/cursosdedesarrollo/app/miapp/controllers/JpaController.java
+++ src/main/java/com/cursosdedesarrollo/app/miapp/controllers/JpaController.java
@@ -1,21 +1,25 @@
 package com.cursosdedesarrollo.app.miapp.controllers;
 
 import com.cursosdedesarrollo.app.miapp.domain.Alumno;
-import com.cursosdedesarrollo.app.miapp.domain.Persona;
 import com.cursosdedesarrollo.app.miapp.repositories.AlumnoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "/jpa")
 public class JpaController {
+
+    Logger logger = LoggerFactory.getLogger(JpaController.class);
     List<Alumno> alumnos;
     @Autowired
     private AlumnoRepository alumnoRepository;
@@ -34,7 +38,7 @@ public class JpaController {
     @GetMapping("/insert")
     public String insert(Model modelo) {
         Integer actualSize= alumnoRepository.findAll().size();
-        System.out.println("/mvc/insert: actualSize="+actualSize);
+        logger.trace("/mvc/insert: actualSize="+actualSize);
         if ( actualSize.equals(0)){
             Alumno alumno = new Alumno();
             alumno.setNombre("Pepe");
@@ -46,6 +50,50 @@ public class JpaController {
         List<Alumno> alumnos = alumnoRepository.findAll();
         modelo.addAttribute("alumnos", alumnos);
         return "listadojpa";
+
+    }
+
+    @GetMapping("/show/{id}")
+    public String insert(Model modelo, @PathVariable("id") Long id) {
+        Optional<Alumno> alumno = this.alumnoRepository.findById(id);
+        if (alumno.isPresent()){
+            modelo.addAttribute("alumno", alumno.get());
+        }else{
+            modelo.addAttribute("alumno", new Alumno());
+        }
+
+        return "showalumno";
+
+    }
+
+    @GetMapping("/update/{id}")
+    public String udpate(Model modelo, @PathVariable("id") Long id) {
+        Optional<Alumno> alumno = this.alumnoRepository.findById(id);
+        if (alumno.isPresent()){
+            Alumno alumnoRegistry = alumno.get();
+            alumnoRegistry.setEdad(44);
+            alumnoRepository.save(alumnoRegistry);
+            modelo.addAttribute("alumno", alumnoRegistry);
+        }else{
+            modelo.addAttribute("alumno", new Alumno());
+        }
+
+        return "showalumno";
+
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(Model modelo, @PathVariable("id") Long id) {
+        Optional<Alumno> alumno = this.alumnoRepository.findById(id);
+        if (alumno.isPresent()){
+            Alumno alumnoRegistry = alumno.get();
+            alumnoRepository.delete(alumnoRegistry);
+            modelo.addAttribute("alumno", alumnoRegistry);
+        }else{
+            modelo.addAttribute("alumno", new Alumno());
+        }
+
+        return "showalumno";
 
     }
 
